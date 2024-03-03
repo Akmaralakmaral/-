a4095ac6fb50f261456d79309a216015dbed6c2d
Akmaral <dzhanyshbekova@gmail.com>
2024-03-04 00:44:58 +0600

    ООП_Лаба6

---
+++ Лаба№6/ООП_лаба6/Laba2/Laba2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+// Родительский класс Display
+class Display {
+protected:
+    char type[32];
+    long colors;
+    int x_resolution;
+    int y_resolution;
+
+public:
+    // Конструктор с аргументами
+    Display(const char* displayType, long displayColors, int xRes, int yRes)
+        : colors(displayColors), x_resolution(xRes), y_resolution(yRes) {
+        strncpy_s(type, displayType, _TRUNCATE);
+    }
+
+    // Размещение информации о классе-родителе на консоли
+    void displayInfo() const {
+        std::cout << "----------------------------------------\n"
+            << "Информация о дисплее\n"
+            << "----------------------------------------\n"
+            << "Тип: " << type << "\nЦвета: " << colors
+            << "\nРазрешение: " << x_resolution << "x" << y_resolution << "\n";
+    }
+};
+// Родительский класс MotherBoard
+class MotherBoard {
+protected:
+    int processor;
+    int speed;
+    int RAM;
+
+public:
+    // Конструктор с аргументами
+    MotherBoard(int proc, int spd, int ram)
+        : processor(proc), speed(spd), RAM(ram) {}
+    // Размещение информации о классе-родителе на консоли
+    void displayInfo() const {
+        std::cout << "----------------------------------------\n"
+            << "Информация о материнской плате\n"
+            << "----------------------------------------\n"
+            << "Процессор: " << processor << "\nСкорость: " << speed
+            << " GHz\nОперативная память: " << RAM << " GB\n";
+    }
+};
+// Класс-потомок Computer
+class Computer : public Display, public MotherBoard {
+private:
+    char name[64];
+    int hard_disk;
+
+public:
+    // Конструктор с аргументами
+    Computer(const char* compName, const char* displayType, long displayColors,
+        int xRes, int yRes, int proc, int spd, int ram, int disk)
+        : Display(displayType, displayColors, xRes, yRes),
+        MotherBoard(proc, spd, ram), hard_disk(disk) {
+        strncpy_s(name, compName, _TRUNCATE);
+    }
+    // Метод для отображения информации о компьютере на экране
+    void show() const {
+        std::cout << "----------------------------------------\n"
+            << "Информация о компьютере\n"
+            << "----------------------------------------\n"
+            << "Имя компьютера: " << name << "\nРазмер жесткого диска: " << hard_disk << " GB\n";
+        Display::displayInfo();
+        MotherBoard::displayInfo();
+    }
+};
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    // Пример использования класса Computer
+    Computer myComputer("MyPC", "LCD", 16777216, 1920, 1080, 4, 3, 16, 500);
+    myComputer.show();
+
+    return 0;
+}
---
+++ Лаба№6/ООП_лаба6/ООП_лаба6/ООП_лаба6.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    string position;
+    double salary;
+
+public:
+    // Конструктор по умолчанию
+    Employee() : name(""), position(""), salary(0.0) {}
+
+    // Конструктор с аргументами
+    Employee(const string& name, const string& position, double salary)
+        : name(name), position(position), salary(salary) {}
+
+    // Деструктор
+    ~Employee() {
+        std::cout << "Объект Employee уничтожен для " << name << endl;
+    }
+
+    // Расчет годовой заработной платы
+    double calculateAnnualSalary() const {
+        return salary * 12;
+    }
+
+    // Вывод информации о работнике на консоль
+    void displayInfo() const {
+        std::cout << "----------------------------------------\n"
+            << "Информация о сотрудниках\n"
+            << "----------------------------------------\n"
+            << "Имя: " << name << "\nДолжность: " << position << "\nЗаработная плата: $" << salary << "\n";
+    }
+};
+
+class Manager : public Employee {
+private:
+    double annual_bonus;
+    string company_car;
+    int stock_options;
+
+public:
+    // Конструктор по умолчанию
+    Manager() : annual_bonus(0.0), company_car(""), stock_options(0) {}
+
+    // Конструктор с аргументами
+    Manager(const string& name, const string& position, double salary,
+        double annual_bonus, const string& company_car, int stock_options)
+        : Employee(name, position, salary), annual_bonus(annual_bonus),
+        company_car(company_car), stock_options(stock_options) {}
+
+    // Деструктор
+    ~Manager() {
+        cout << "\nОбъект Manager уничтожен для " << name << endl;
+    }
+
+    // Вывод информации о менеджере на консоль
+    void displayInfo() const {
+        Employee::displayInfo();  // Вызываем метод родительского класса
+        cout << "Годовой бонус: " << annual_bonus << "\nКомпанийская машина: " << company_car
+            << "\nОпции по акциям: " << stock_options << endl;
+    }
+    double getTotalAnnualEarnings() const {
+        // Вызываем метод calculateAnnualSalary базового класса и добавляем к нему annual_bonus
+        return calculateAnnualSalary() + annual_bonus;
+    }
+
+};
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    Employee emp("Джон Доу", "Инженер-программист", 5000.0);
+    emp.displayInfo();
+    cout << "Годовая зарплата: $" << emp.calculateAnnualSalary() << endl;
+
+    Manager manager("Алиса Смит", "Руководитель проекта", 7000.0, 2000.0, "Компанийская машина ABC", 500);
+    manager.displayInfo();
+    cout << "Годовая зарплата: $" << manager.calculateAnnualSalary() << endl;
+    cout << "Итоговая годовая зарплата: $" << manager.getTotalAnnualEarnings() << endl;
+
+    Manager manager2("Bob Johnson", "Engineering Manager", 8000.0, 2500.0, "Company Car XYZ", 600);
+    manager2.displayInfo();
+    std::cout << "Annual Salary: $" << manager2.calculateAnnualSalary() << "\n";
+    cout << "Итоговая годовая зарплата: $" << manager2.getTotalAnnualEarnings() << endl;
+
+    return 0;
+}
